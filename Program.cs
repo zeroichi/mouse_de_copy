fe589a9c917e9c0cfe902cc8d406562fd65e23d4
zeroichi <zeroichi0@gmail.com>
2013-12-09 18:16:45 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace mouse_de_copy
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            CaptureAndCopy();
+        }
+
+        static void CaptureAndCopy()
+        {
+            double ratio = 0.5;
+            var f = new Selector();
+
+            // ディスプレイが複数ある場合でも全画面を覆うようにウィンドウサイズを調整
+            int left, top, right, bottom;
+            left = Screen.AllScreens.Min((s) => s.Bounds.Left);
+            top = Screen.AllScreens.Min((s) => s.Bounds.Top);
+            right = Screen.AllScreens.Max((s) => s.Bounds.Right);
+            bottom = Screen.AllScreens.Max((s) => s.Bounds.Bottom);
+            f.Left = left;
+            f.Top = top;
+            f.Width = right - left;
+            f.Height = bottom - top;
+
+            f.ShowDialog();
+            var end = f.end;
+            var start = f.start;
+            Bitmap bmp = new Bitmap(end.X - start.X, end.Y - start.Y);
+            Bitmap bmp2 = new Bitmap((int)(bmp.Width * ratio), (int)(bmp.Height * ratio));
+            f.Dispose();
+            using (var g = Graphics.FromImage(bmp))
+                g.CopyFromScreen(f.start, new Point(0, 0), new Size(bmp.Width, bmp.Height));
+            using (var g = Graphics.FromImage(bmp2))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(bmp, new Rectangle(0, 0, bmp2.Width, bmp2.Height), new Rectangle(0, 0, bmp.Width, bmp.Height), GraphicsUnit.Pixel);
+            }
+            Clipboard.SetImage(bmp2);
+            bmp.Dispose();
+            bmp2.Dispose();
+        }
+    }
+}
---
+++ Selector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mouse_de_copy
+{
+    public partial class Selector : Form
+    {
+        public Point start, end;
+
+        public Selector()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_MouseDown(object sender, MouseEventArgs e)
+        {
+            start = this.PointToScreen(new Point(e.X, e.Y));
+            pictureBox1.Visible = true;
+            this.Capture = true;
+        }
+
+        private void Form2_MouseUp(object sender, MouseEventArgs e)
+        {
+            end = this.PointToScreen(new Point(e.X, e.Y));
+            if (start.X > end.X)
+            {
+                var temp = start.X;
+                start.X = end.X;
+                end.X = temp;
+            }
+            if (start.Y > end.Y)
+            {
+                var temp = start.Y;
+                start.Y = end.Y;
+                end.Y = temp;
+            }
+            this.Close();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Selector_MouseMove(object sender, MouseEventArgs e)
+        {
+            end = this.PointToScreen(new Point(e.X, e.Y));
+
+            pictureBox1.Top = start.Y < end.Y ? start.Y : end.Y;
+            pictureBox1.Left = start.X < end.X ? start.X : end.X;
+            pictureBox1.Width = Math.Abs(start.X - end.X);
+            pictureBox1.Height = Math.Abs(start.Y - end.Y);
+        }
+    }
+}
